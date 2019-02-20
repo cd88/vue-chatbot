635e036083640688ea7c94ceab1a67e8a0cd8ab9
celloworld <camerondonahue@gmail.com>
2019-02-19 18:01:13 -0800

    added/bootstrapped basic components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChatItem msg="Chatbot isn't available right now"/>
+    <ChatEntry />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatItem from './components/ChatItem.vue'
+import ChatEntry from './components/ChatEntry.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ChatItem,
+    ChatEntry
   }
 }
 </script>
---
+++ src/components/ChatEntry.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="chat-entry-form">
+    <input placeholder="works" />
+    <button type="button" name="button">Send</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ChatEntry',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
